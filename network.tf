f9b561690ef5e50ade4c0a77f1cb86c5d2cc61dc
YFanSh1011 <89020588+YFanSh1011@users.noreply.github.com>
2024-03-25 12:50:10 +1100

    Add private subnets and associated resources

--- network.tf
+++ network.tf
@@ -128,3 +128,69 @@ resource "aws_nat_gateway" "public_b" {
     map("Name", "${local.prefix}-public-b")
   )
 }
+
+
+#####################################################
+#  Private Subnets - Outbound Internet Access Only  #
+#####################################################
+resource "aws_subnet" "private_a" {
+  cidr_block        = "10.0.10.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table" "private_a" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-a")
+  )
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private_a.id
+}
+
+resource "aws_route" "private_a_internet_outbound" {
+  route_table_id         = aws_route_table.private_a.id
+  nat_gateway_id         = aws_nat_gateway.public_a.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_subnet" "private_b" {
+  cidr_block        = "10.0.11.0/24"
+  vpc_id            = aws_vpc.main.id
+  availability_zone = "${data.aws_region.current.name}b"
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-private-b")
+  )
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private_b.id
+}
+
+resource "aws_route" "private_b_internet_outbound" {
+  route_table_id         = aws_route_table.private_b.id
+  nat_gateway_id         = aws_nat_gateway.public_b.id
+  destination_cidr_block = "0.0.0.0/0"
+}
